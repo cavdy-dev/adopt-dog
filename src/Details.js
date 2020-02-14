9474b57ad6d32737588c4700aeb79ae6137664b1
Cavdy <cavdyofficials@gmail.com>
2020-02-14 14:35:28 +0100

    add portals in react

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
+import { navigate } from "@reach/router";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
 
 class Details extends Component {
   state = {
-    loading: true
+    loading: true,
+    showModal: false
   };
 
   componentDidMount() {
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -22,18 +27,53 @@ class Details extends Component {
     }, console.error);
   }
 
+  toggleModal = () => {
+    const { showModal } = this.state;
+    this.setState({ showModal: !showModal });
+  };
+
+  adopt = () => navigate(this.state.url);
+
   render() {
     if (this.state.loading) return <h1>Loading...</h1>;
 
-    const { name, animal, location, description, media, breed } = this.state;
+    const {
+      name,
+      animal,
+      location,
+      description,
+      media,
+      breed,
+      showModal
+    } = this.state;
     return (
       <div className="details">
         <Carousel media={media} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No, I am a monster</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
--- src/Pet.js
+++ src/Pet.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "@reach/router";
 
 const Pet = ({ name, animal, breed, media, location, id }) => {
   let hero = "https://placecorgi.com/300/300";
@@ -7,7 +8,7 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
     hero = media[0].small;
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,7 +16,7 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 };
 
